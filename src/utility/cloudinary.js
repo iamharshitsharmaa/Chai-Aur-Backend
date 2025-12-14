24f004a38bbfda7dd4a840decc7d1ee81666f649
iamharshitsharmaa <harshitsharma7222@gmail.com>
2025-12-14 21:34:41 +0530

    file uploads with cloudinary and multer

---
+++ src/utility/cloudinary.js
@@ -0,0 +1,25 @@
+import { v2 as cloudinary }  from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.cloud_name,
+    api_key: process.env.api_key,
+    api_secret: process.env.api_secret
+});
+
+const uploadToCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        const response = await cloudinary.uploader.upload(localFilePath, 
+            {resource_type : "auto"}
+        ) 
+        console.log("File uploaded to Cloudinary successfully", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        console.error("Error uploading file to Cloudinary", error);
+        throw error;
+    }
+}
+
+export { uploadToCloudinary };
